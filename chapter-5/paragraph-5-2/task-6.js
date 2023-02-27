7047ab9dd391ce144259f9ffa2d1b7dc06c8b7d1
Viktorphp84 <viktorphp@yandex.ru>
2023-02-27 16:54:04 +0300

    added tasks up to 5 chapter

---
+++ chapter-5/paragraph-5-2/task-6.js
@@ -0,0 +1,4 @@
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max - min);
+    return Math.round(rand);
+}
---
+++ chapter-5/paragraph-5-3/task-1.js
@@ -0,0 +1,3 @@
+function ucFirst(str) {
+    return str.at(0).toUpperCase() + str.slice(1);
+}
